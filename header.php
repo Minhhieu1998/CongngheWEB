122a85aa36352ec375fc2575192b64d658597869
Minhhieu1998 <36042226+Minhhieu1998@users.noreply.github.com>
2018-10-10 21:08:35 +0700

    Add files via upload

---
+++ header.php
@@ -0,0 +1,101 @@
+<!DOCTYPE html>
+<html>
+<title>QUẢN LÝ SINH VIÊN</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="css/trangchu.css">
+<link rel="stylesheet" href="css/anh.css">
+<link rel="stylesheet" href="css/menu.css">
+<link rel="stylesheet" href="css/footer.css">
+<style>
+body,h1,h2,h3,h4,h5,h6 {font-family: "Raleway", Arial, Helvetica, sans-serif}
+</style>
+<body class="w3-light-grey">
+    <div id="menu">
+  <ul>
+    <div class="logo">
+
+    <a href="trangchu.html" target="_blank"><img src="lib/rong.jpg"></a>
+    </div>
+
+    <li><a href="trangchu.html">Trang chủ</a></li>
+    <li>  <a href="#tencacdiadiem">Quản lý sinh viên</a>
+
+    </li>
+    <li><a href="#lienhect">Sơ đồ lớp</a>
+
+    </li>
+    </li>
+    <li><a href="#lienhect">Bảng tin</a>
+
+    </li>
+    <li><a href="logout.php">Đăng xuất</a></li>
+  </ul>
+</div>
+<!-- Header -->
+<header class="w3-display-container w3-content" style="max-width:1500px;">
+  <img class="w3-image" src="lib/anh3.jpg" alt="The Hotel" style="min-width:1000px" width="1500" height="800">
+  <div class="w3-display-left w3-padding w3-col l6 m8">
+    <div class="w3-container w3-red">
+      <h2><i class="fa fa-bed w3-margin-right"></i>TÌM SINH VIÊN</h2>
+    </div>
+    <div class="w3-container w3-white w3-padding-16">
+      <form action="/action_page.php" target="_blank">
+        <div class="w3-row-padding" style="margin:0 -16px;">
+          <div class="w3-half w3-margin-bottom">
+            <label><i class="fa fa-calendar-o"></i> LỚP</label>
+            <input class="w3-input w3-border" type="text" placeholder="...." name="CheckIn" required>
+          </div>
+           <div class="w3-half w3-margin-bottom">
+            <label><i class="fa fa-calendar-o"></i> Tên sinh viên</label>
+            <input class="w3-input w3-border" type="text" placeholder="...." name="CheckIn" required>
+          </div>
+
+        </div>
+     
+
+        <button class="w3-button w3-dark-grey" type="submit"><i class="fa fa-search w3-margin-right"></i> Search</button>
+      </form>
+    </div>
+  </div>
+</header>
+<body>
+  <div>
+    
+  </div>
+  <p>
+    NỘI DUNG
+  </p> <br>
+  <p>CONTENT</p><br>
+  <p>CONTENT</p><br>
+  <p>CONTENT</p><br>
+  <p>CONTENT</p><br>
+  <p>CONTENT</p><br>
+  <p>CONTENT</p><br>
+
+  <div class="w3-container" id="contact">
+  <h2 id=lienhect>LIÊN HỆ</h2>
+  <p>NẾU CÓ THẮC MẮC GÌ ,ĐỪNG NGẦN NGẠI HÃY LIÊN LAC VỚI CHÚNG TÔI.</p>
+  <i class="fa fa-map-marker w3-text-red" style="width:30px"></i> HÀ NỘI ,VIỆT NAM<br>
+  <i class="fa fa-phone w3-text-red" style="width:30px"></i> Phone: +00 151515<br>
+  <i class="fa fa-envelope w3-text-red" style="width:30px"> </i> Email: ntminhhieu98@gmail.com<br>
+  <form action="/action_page.php" target="_blank">
+    <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Name" required name="Name"></p>
+    <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Email" required name="Email"></p>
+    <p><input class="w3-input w3-padding-16 w3-border" type="text" placeholder="Message" required name="Message"></p>
+    <p><button class="w3-button w3-black w3-padding-large" type="submit">SEND MESSAGE</button></p>
+  </form>
+</div>
+  <!-- Footer -->
+<footer class="w3-padding-32 w3-black w3-center w3-margin-top">
+  <h5>Find Us On</h5>
+  <div class="w3-xlarge w3-padding-16">
+    <i class="fa fa-facebook-official w3-hover-opacity" ></i>
+    <i class="fa fa-instagram w3-hover-opacity"></i>
+
+  </div>
+  <p>Powered by <a href="https://plus.google.com/u/0/106974998444610829545" target="_blank" class="w3-hover-text-green">ntminhhieu98@gmail.com</a></p>
+</footer>
+</body>
+
+</html>
